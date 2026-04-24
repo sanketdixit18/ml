b314ed6537a394e0411cd810eb8a8be029c2585a
sanketdixit18 <sanketdixit424@gmail.com>
2026-04-24 18:54:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -397,6 +397,7 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("ML_PORT", 5000))
+    # port = int(os.environ.get("ML_PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     print(f"[ML] Server running → http://localhost:{port}")
     app.run(host="0.0.0.0", port=port, debug=False)
